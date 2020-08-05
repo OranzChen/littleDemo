845ac1aa0400d77d30e65f2c522d979a1291d94b
unknown <W9005797@adc.com>
2020-08-05 17:47:18 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="tsx">
+import { createComponent, onMounted, ref, reactive, watch, computed } from '@vue/composition-api'
+export default createComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
